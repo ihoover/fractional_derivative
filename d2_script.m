e787ef2e9e37d9ad4653ae5754ba7eece9ffe5d6
Dimitar Dimitrov <dimitar.dimitrov@students.olin.edu>
2015-04-23 22:05:10 -0400

    works

--- d2_script.m
+++ d2_script.m
@@ -1,12 +1,14 @@
-f = (1/24)*x^4; %function
-start_d=0; %start derivative
-end_d=4; %end derivative
-s = 60; %number of steps
+syms x
+f = (1/2)*x^2; %function
+start_d=-2; %start derivative
+end_d=3; %end derivative
+s = 100; %number of steps
 o=5; %taylor series order
 
 clf;
 for a=linspace(start_d,end_d,s+1)
     ezplot(d2(f,a,o))
+    text('Position',[2.5,2.5],'HorizontalAlignment','center','String',strcat('$\frac{d^{',num2str(a),'}}{dx^{',num2str(a),'}}[',latex(f),']$'),'interpreter','latex','fontsize',20)
     axis([0 5 -1 3])
     drawnow
 end
